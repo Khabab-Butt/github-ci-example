7d58f52c62606b0b8f837cfdae19ffc5c45db340
Khabab <khababbutt1228@gmail.com>
2025-01-08 20:00:36 +0500

    Initial Commit

---
+++ com.example.CIExample1/src/main/java/com/example/CIExample1/App.java
@@ -0,0 +1,12 @@
+package com.example.CIExample1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public String sayHello() {
+		return "Hello";
+	}
+}
---
+++ com.example.CIExample1/src/test/java/com/example/CIExample1/AppTest.java
@@ -0,0 +1,28 @@
+package com.example.CIExample1;
+
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class AppTest {
+
+	private App app;
+
+	@Before
+	public void setup() {
+		app = new App();
+	}
+
+	@Test
+	public void testSayHello() {
+		assertEquals("Hello", app.sayHello());
+	}
+}
